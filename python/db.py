777b4f724bca28d6ee60276670cbab5e785ac0d1
wind <jamwujustyle@gmail.com>
2024-11-16 13:24:00 +0500

    connected to database with table logging data values: uuid email hashed pass

---
+++ python/db.py
@@ -0,0 +1,28 @@
+import psycopg2
+import logging 
+def log_config():
+    logging.basicConfig(
+        level=logging.DEBUG, format="  %(name)s - %(levelname)s - %(message)s"
+        # logging.debug
+        # logging.info
+        # logging.warning
+        # logging.error
+        # logging.critical
+    )
+log_config()
+
+def connect():
+    conn = psycopg2.connect(
+        dbname= 'to_do',
+        host= 'localhost',
+        password= '0880',
+        user= 'postgres',
+        port= 5432
+    )
+    try: 
+        cursor = conn.cursor()
+        logging.info('successfully connected to database')
+        
+    except Exception as ex: 
+        print(f'error connecting to database: {ex}')
+connect()
